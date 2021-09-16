090b192ae894d8e5d74ac5a6eeeee859ec6d5310
Evan Flack <evanflack@Evans-MacBook-Pro.local>
2021-09-16 10:33:57 -0700

    cleaned up script

--- src/01_sample/01_combine_data_sources.R
+++ src/01_sample/01_combine_data_sources.R
@@ -1,23 +1,23 @@
-# ------------------------------------------------------------------------------
-# Proj: Umpire Fixed Effects
+# Header -----------------------------------------------------------------------
+# Proj: Umpire FE
 # Author: Evan Flack (evanjflack@gmail.com)
-# Desc: Reads in and combines pitch, atbat, and game datasets downloaded from
+# Desc: Reads in and combines pitch, at-bat, and game data sets downloaded from
 #       Kaggle
-# ------------------------------------------------------------------------------
 
 # Libraries --------------------------------------------------------------------
 library(data.table)
 library(magrittr)
 library(stringr)
-library(cfo.behavioral)
+library(tictoc)
 
+# Define user functions
 source("../supporting_code/define_functions.R")
 
 start_log_file("log/01_combine_data_sources")
 
 # Read in Data -----------------------------------------------------------------
-message("Reading in data...")
 
+# All from 2015-2018
 # Pitch level
 pitch_dt <- fread("../../data/kaggle_data/pitches.csv")
 # At-bat level
@@ -26,9 +26,8 @@ atbat_dt <- fread("../../data/kaggle_data/atbats.csv")
 game_dt <- fread("../../data/kaggle_data/games.csv")
 
 # Prep Data --------------------------------------------------------------------
-message("Prepping data...")
 
-# Separate date infomation
+# Separate date information
 game_dt %<>%
   .[, `:=`(year = substr(date, 1, 4), 
            month = substr(date, 6, 7), 
@@ -46,15 +45,21 @@ atbat_dt_final <- atbat_dt %>%
   .[, .(g_id, ab_id, batter_id, inning, top)]
 
 # Merge Data -------------------------------------------------------------------
-message("Merging data...")
+# Merge all 3 data sets, check to see no observations were lost
 
+n1 <- nrow(pitch_dt_final)
 model_dt <- pitch_dt_final %>% 
   merge(atbat_dt_final, by = "ab_id") %>% 
   merge(game_dt_final, by = "g_id")
+n2 <- nrow(model_dt)
 
 # Export -----------------------------------------------------------------------
-message("Exporting...")
 
-fwrite(model_dt, paste0("../../data/out/reg_season_data_2015_2018.csv"))
+if (n1 != n2) {
+  message("OBSERVATIONS LOST IN MERGING, DATA NOT EXPORTED!!")
+} else if (n1 == n2) {
+  message("Data exported to csv.")
+  fwrite(model_dt, paste0("../../data/out/reg_season_data_2015_2018.csv"))
+}
 
 end_log_file()
