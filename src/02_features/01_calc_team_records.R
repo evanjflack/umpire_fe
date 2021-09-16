5922d21f4b686ec0f82faf4df04cdeb4a84e5b9b
Evan Flack <evanjflack@gmail.com>
2021-09-16 13:29:48 -0700

    cleaned up wins per team

--- src/02_features/01_calc_team_records.R
+++ src/02_features/01_calc_team_records.R
@@ -1,15 +1,14 @@
-# ------------------------------------------------------------------------------
+# Header -----------------------------------------------------------------------
 # Proj: Umpire Fixed Effects
 # Author: Evan Flack (evanjflack@gmail.com)
 # Desc: Calculates team records on a given day/year (to be used in randomization 
 #       check)
-# ------------------------------------------------------------------------------
 
 # Libraries --------------------------------------------------------------------
 library(data.table)
 library(magrittr)
 library(lubridate)
-library(cfo.behavioral)
+library(tictoc)
 
 source("../supporting_code/define_functions.R")
 
@@ -18,12 +17,14 @@ start_log_file("log/01_calc_team_records")
 # Read in Data -----------------------------------------------------------------
 message("Reading in data...")
 
+# Game data from 2015-2018
 game_dt <- fread("../../data/kaggle_data/games.csv")
 
 # Team Records -----------------------------------------------------------------
 message("Calculating team records...")
-# Reshaping teams.wins to long
+
 wins_long <- game_dt %>% 
+  # Define binary variables for if the home team or away team won
   .[, away_win := ifelse(away_final_score > home_final_score, 1, 0)] %>% 
   .[, home_win := 1 - away_win] %>% 
   .[, .(g_id, date, away_win, home_win)] %>% 
@@ -31,26 +32,31 @@ wins_long <- game_dt %>%
   .[, team1 := ifelse(grepl("away", variable), "away", "home")] %>% 
   .[, variable := NULL]
 
+# Determine which team was home/away for each game
 teams_long <- game_dt %>% 
   .[, .(g_id, date, away_team, home_team)] %>% 
   melt(id.var = c("g_id", "date"), value.name = "team") %>% 
   .[, team1 := ifelse(grepl("away", variable), "away", "home")] %>% 
   .[, variable := NULL]
 
+# Combine the team names with the wins, reshape to wide, and calculate the
+# win percentage of each team before the game in question
 teams_wins_long <- teams_long %>% 
   merge(wins_long, by = c("g_id", "date", "team1")) %>% 
   .[, year := year(date)] %>% 
   .[order(team, year, g_id)] %>% 
   .[, cum_wins := ave(win, team, year, FUN=cumsum)] %>% 
+  .[cum_wins > 0, cum_wins := cum_wins - 1] %>% 
   .[, games := seq(1, .N), by = .(team, year)] %>% 
+  .[games > 1, games := games - 1] %>% 
   .[, win_perc := cum_wins/games]
 
-
-team_wins_wide <- teams_wins_long %>% 
+# Cast to wide
+team_wins_wide <- teams_wins_long %>%
   dcast(g_id ~ team1, value.var = c("cum_wins", "win_perc"))
 
 # Export -----------------------------------------------------------------------
-message("Exporting...")
+message("Exporting to csv.")
 
 fwrite(team_wins_wide, paste0("../../data/out/team_wins.csv"))
 
